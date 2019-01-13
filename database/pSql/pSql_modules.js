d31b8fa13eed7b7b826dd1bdd1b34c598442e282
Ammar <ammar.alnatsheh@gmail.com>
2019-01-12 22:26:22 -0800

    improve the pSql module/ seperation of concern

--- database/pSql/pSql_modules.js
+++ database/pSql/pSql_modules.js
@@ -1,23 +1,47 @@
 const pSql = require('./postgres_index');
 
 function selectProductPSql(id, callback) {
-  pSql.query('SELECT * FROM products WHERE id = ($1)', [id], callback);
+  pSql.query('SELECT * FROM products WHERE id = ($1)', [id], (err, data) => {
+    if (err) {
+      callback(err.stack);
+    } else {
+      callback(null, data.rows[0]);
+    }
+  });
 }
 
 function selectPhotosPSql(id, callback) {
-  pSql.query('SELECT * FROM photos WHERE id = ($1)', [id], callback);
+  pSql.query('SELECT * FROM photos WHERE id = ($1)', [id], (err, data) => {
+    if (err) {
+      callback(err.stack);
+    } else {
+      callback(null, data.rows[0]);
+    }
+  });
 }
 
 function insertProductPSql(values, callback) {
   const array = [values.product_title || '', values.vendor_name || '', values.review_average || 0,
     values.review_count || 0, values.answered_questions || 0, values.list_price || '', values.discount || '',
     values.price || '', values.prime || 0, values.description || ''];
-  pSql.query('INSERT INTO products (product_title, vendor_name, review_average, review_count, answered_questions, list_price, discount, price, prime, description) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', array, callback);
+  pSql.query('INSERT INTO products (product_title, vendor_name, review_average, review_count, answered_questions, list_price, discount, price, prime, description) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)', array, (err, data) => {
+    if (err) {
+      callback(err.stack);
+    } else {
+      callback(null, data.rows[0]);
+    }
+  });
 }
 
 function insertPhotosPSql(values, callback) {
   const array = [values.main_url || '', values.zoom_url || '', values.product_id || 0, values.main_photo || 0];
-  pSql.query('INSERT INTO photos (main_url, zoom_url, product_id, main_photo) VALUES ($1, $2, $3, $4)', array, callback);
+  pSql.query('INSERT INTO photos (main_url, zoom_url, product_id, main_photo) VALUES ($1, $2, $3, $4)', array, (err, data) => {
+    if (err) {
+      callback(err.stack);
+    } else {
+      callback(null, data.rows[0]);
+    }
+  });
 }
 
 function updateProductPSql(values, callback) {
@@ -25,23 +49,47 @@ function updateProductPSql(values, callback) {
     values.review_count || 0, values.answered_questions || 0, values.list_price || '', values.discount || '',
     values.price || '', values.prime || 0, values.description || ''];
   if (array[0] !== 0) {
-    pSql.query('UPDATE products SET product_title = ($2), vendor_name = ($3), review_average = ($4), review_count = ($5), answered_questions = ($6), list_price = ($7), discount = ($8), price = ($9), prime = ($10), description = ($11) WHERE id = ($1)', array, callback);
+    pSql.query('UPDATE products SET product_title = ($2), vendor_name = ($3), review_average = ($4), review_count = ($5), answered_questions = ($6), list_price = ($7), discount = ($8), price = ($9), prime = ($10), description = ($11) WHERE id = ($1)', array, (err, data) => {
+      if (err) {
+        callback(err.stack);
+      } else {
+        callback(null, data.rows[0]);
+      }
+    });
   }
 }
 
 function updatePhotosPSql(values, callback) {
   const array = [values.id || 0, values.main_url || '', values.zoom_url || '', values.product_id || 0, values.main_photo || 0];
   if (array[0] !== 0) {
-    pSql.query('UPDATE photos SET main_url = ($2), zoom_url = ($3), product_id = ($4), main_photo = ($5) WHERE id = ($1)', array, callback);
+    pSql.query('UPDATE photos SET main_url = ($2), zoom_url = ($3), product_id = ($4), main_photo = ($5) WHERE id = ($1)', array, (err, data) => {
+      if (err) {
+        callback(err.stack);
+      } else {
+        callback(null, data.rows[0]);
+      }
+    });
   }
 }
 
 function deleteProductPSql(id, callback) {
-  pSql.query('DELETE FROM products WHERE id = ($1)', [id], callback);
+  pSql.query('DELETE FROM products WHERE id = ($1)', [id], (err, data) => {
+    if (err) {
+      callback(err.stack);
+    } else {
+      callback(null, data.rows[0]);
+    }
+  });
 }
 
 function deletePhotosPSql(id, callback) {
-  pSql.query('DELETE FROM photos WHERE id = ($1)', [id], callback);
+  pSql.query('DELETE FROM photos WHERE id = ($1)', [id], (err, data) => {
+    if (err) {
+      callback(err.stack);
+    } else {
+      callback(null, data.rows[0]);
+    }
+  });
 }
 
 module.exports = {
