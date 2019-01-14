f130453935283a320f1ebe60da4ce54581f1dd90
Ammar <ammar.alnatsheh@gmail.com>
2019-01-13 22:12:27 -0800

    finish the mongo module with auto increment feature

--- database/mongo/mongo_modules.js
+++ database/mongo/mongo_modules.js
@@ -1,35 +1,53 @@
 const mongoDB = require('./mongo_index');
 
-function selectProductMongoDB(id, callback) {
-  mongoDB.Products.find({ 'id': id }, callback);
+function selectProductMongoDB(values, callback) {
+  mongoDB.Products.find({ _id: values.id }, callback);
 }
 
-function selectPhotosMongoDB(id, callback) {
-  mongoDB.Photos.find({ 'id': id }, callback);
+function selectPhotosMongoDB(values, callback) {
+  mongoDB.Photos.find({ _id: values.id }, callback);
 }
 
 function insertProductMongoDB(values, callback) {
-  mongoDB.Products.insertOne(values, callback);
+  const product = new mongoDB.Products({
+    product_title: values.product_title || '',
+    vendor_name: values.vendor_name || '',
+    review_average: values.review_average || 0,
+    review_count: values.review_count || 0,
+    answered_questions: values.answered_questions || 0,
+    list_price: values.list_price || '',
+    discount: values.discount || '',
+    price: values.price || '',
+    prime: values.prime || '',
+    description: values.description || '',
+  });
+  product.save(callback);
 }
 
 function insertPhotosMongoDB(values, callback) {
-  mongoDB.Photos.insertOne(values, callback);
+  const photo = new mongoDB.Photos({
+    main_url: values.main_url || '',
+    zoom_url: values.zoom_url || '',
+    product_id: values.product_id || 0,
+    main_photo: values.main_photo || 0,
+  });
+  photo.save(callback);
 }
 
 function updateProductMongoDB(values, callback) {
-  mongoDB.Products.updateMany({ 'id': values.id }, values, callback);
+  mongoDB.Products.updateMany({ _id: values.id }, values, callback);
 }
 
 function updatePhotosMongoDB(values, callback) {
-  mongoDB.Photos.updateMany({ 'id': values.id }, values, callback);
+  mongoDB.Photos.updateMany({ _id: values.id }, values, callback);
 }
 
-function deleteProductMongoDB(id, callback) {
-  mongoDB.Products.deleteMany({ 'id': id }, callback);
+function deleteProductMongoDB(values, callback) {
+  mongoDB.Products.deleteMany({ _id: values.id }, callback);
 }
 
-function deletePhotosMongoDB(id, callback) {
-  mongoDB.Photos.deleteMany({ 'id': id }, callback);
+function deletePhotosMongoDB(values, callback) {
+  mongoDB.Photos.deleteMany({ _id: values.id }, callback);
 }
 
 module.exports = {
